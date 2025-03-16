510a7acd680792158fa251c387b2536fbef14c5a
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-16 19:28:15 -0300

    att

--- src/parser/normal_flags/fdeclare.parsser.h
+++ src/parser/normal_flags/fdeclare.parsser.h
@@ -5,4 +5,6 @@
 
 int privateCArgv_parser_get_flag_identifier_start_size(CArgvParse *self,const char *flag,int flag_size);
 
+int CArgvPars_get_flag_size(CArgvParse *self,const char **flags,int flags_size);
+
 const char * CArgvPars_get_flag(CArgvParse *self ,const char **flags,int flags_size, int index);
