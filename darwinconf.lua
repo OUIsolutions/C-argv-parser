ba7df3257b7657d052cc6e9d5c460bb610cd444e
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-16 17:09:46 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -2,5 +2,5 @@
 darwin.silverchain.generate({
     src = "src",
     project_short_cut = "doTHeWorld",
-    tags = {  "dep_declare", "consts", "macros", "types", "globals", "fdeclare", "fdefine" }
+    tags = { "macros",  "consts", "types", "globals", "fdeclare", "fdefine" }
 })
---
+++ src/one.c
@@ -0,0 +1,5 @@
+
+#ifndef C_ARGV_PARSER_ONE
+#define C_ARGV_PARSER_ONE
+#include "imports/imports.fdefine.h"
+#endif 
