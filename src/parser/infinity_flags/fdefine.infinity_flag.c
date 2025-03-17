925d08335fc2fa2ac6233c46d37e99d479b55195
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-16 22:56:36 -0300

    removing trash

--- src/parser/infinity_flags/fdefine.infinity_flag.c
+++ src/parser/infinity_flags/fdefine.infinity_flag.c
@@ -1,9 +1,3 @@
-//silver_chain_scope_start
-//DONT MODIFY THIS COMMENT
-//this import is computationally generated
-//mannaged by SilverChain: https://github.com/OUIsolutions/SilverChain
-#include "../../imports/imports.globals.h"
-//silver_chain_scope_end
 
 
 int CArgvParse_get_infinity_flag_size(CArgvParse *self,const char **flags,int flags_size){
