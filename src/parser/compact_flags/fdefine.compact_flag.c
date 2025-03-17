0ac978f9dd220ecfc286ca4dac5dde7a3576b708
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-16 21:54:45 -0300

    created compact flags system

--- src/parser/compact_flags/fdefine.compact_flag.c
+++ src/parser/compact_flags/fdefine.compact_flag.c
@@ -9,82 +9,40 @@
 
 
 int CArgvParse_get_compact_flag_size(CArgvParse *self,const char **flags,int flags_size){
-    int found=0;
-    for(int i = 0; i < self->total_args-1; i++){
-
-
+    int size = 0;
+    for(int i = 0; i < self->total_args; i++){
         const char *current_arg = self->args[i];
         int current_arg_size = privateArgv_parser_string_size(current_arg);
-        int identifier_start_size = privateCArgv_parser_get_flag_identifier_start_size(self,current_arg,current_arg_size);        
-        c_argv_bool its_flag = identifier_start_size != -1;
-        //means its not a flag
-        if(!its_flag){
-            continue;
-        }
-        const char *formmated_flag_comparaton_flag = current_arg + identifier_start_size;
-        int formmated_flag_comparaton_flag_size = current_arg_size-identifier_start_size;
-
-        for(int j = 0; j < flags_size;j++){
+        for(int j=0; j < flags_size;j++){
             const char *flag = flags[j];
             int flag_size = privateArgv_parser_string_size(flag);
-
-            c_argv_bool is_the_current_flag = privateArgv_strings_equals(
-                formmated_flag_comparaton_flag,
-                formmated_flag_comparaton_flag_size,
-                flag,
-                flag_size
-            );
-            if(is_the_current_flag){
-                found++;
-                break;
+        
+            if(privateArgv_parsser_starts_with(current_arg,current_arg_size,flag,flag_size)){
+                size++;
             }
         }
-
     }
-    return found;
+    return size;
 }    
 
 
 const char * CArgvParse_get_compact_flag(CArgvParse *self ,const char **flags,int flags_size, int index){
     int found=0;
-    for(int i = 0; i < self->total_args-1; i++){
-
+    for(int i = 0; i < self->total_args; i++){
 
         const char *current_arg = self->args[i];
         int current_arg_size = privateArgv_parser_string_size(current_arg);
-        int identifier_start_size = privateCArgv_parser_get_flag_identifier_start_size(self,current_arg,current_arg_size);        
-        c_argv_bool its_flag = identifier_start_size != -1;
-        //means its not a flag
-        if(!its_flag){
-            continue;
-        }
-        const char *formmated_flag_comparaton_flag = current_arg + identifier_start_size;
-        int formmated_flag_comparaton_flag_size = current_arg_size-identifier_start_size;
-
-        for(int j = 0; j < flags_size;j++){
+        for(int j=0; j < flags_size;j++){
             const char *flag = flags[j];
             int flag_size = privateArgv_parser_string_size(flag);
-
-            c_argv_bool is_the_current_flag = privateArgv_strings_equals(
-                formmated_flag_comparaton_flag,
-                formmated_flag_comparaton_flag_size,
-                flag,
-                flag_size
-            );
-            if(is_the_current_flag){
-
+        
+            if(privateArgv_parsser_starts_with(current_arg,current_arg_size,flag,flag_size)){
                 if(found == index){
-
-                    private_CArgvParse_add_used(self,i);
-                    private_CArgvParse_add_used(self,i+1);
-                    const  char *next = self->args[i+1];
-                    return next;
+                    return current_arg + flag_size;
                 }
                 found++;
-                break;
             }
         }
-
     }
     return C_ARGV_PARSER_NULL;
 }
